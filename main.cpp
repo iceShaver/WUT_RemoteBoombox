41b64a19bb9b5c1ecb392dd74f0ce1cf1eb46f3d
kamil <iceShaver@users.noreply.github.com>
2023-09-01 18:56:38 +0200

    Changed project type to Qt Widgets Application

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
-#include "apploaderui.h"
+#include "mainwindow.h"
 
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "RemoteBoombox_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    AppLoaderUi w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
